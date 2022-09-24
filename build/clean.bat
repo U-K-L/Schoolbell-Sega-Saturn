738323daa16d2a9a1e5d4b684a39c949ae9a357f
U.K.L <WilliamsNoah1720@gmail.com>
2022-09-24 03:03:34 -0400

    Added additional files and clean up

--- build/clean.bat
+++ build/clean.bat
@@ -4,8 +4,8 @@ SET PATH=%COMPILER_DIR%\SH_COFF\Other Utilities;%PATH%
 
 rm -f ./cd/0.bin
 rm -f *.o
-rm -f ../source/*.o
+rm -f ../src/source/*.o
 rm -f ../ZTE/*.o
-rm -f ./sl_coff.bin
-rm -f ./sl_coff.coff
-rm -f ./sl_coff.map
+rm -f ./SCHOOL-BELL.bin
+rm -f ./SCHOOL-BELL.coff
+rm -f ./SCHOOL-BELL.map
